fd364aa0224c5cd586a5b3ee6c5e080c16ce78c7
rahad404 <rahad.0911@gmail.com>
2026-05-20 03:32:38 +0600

    add: function for updating testuser entry by id

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.get('/', (req, res) => {
    res.send('express server is running!')
-})
+});
 
 // test user creation route
 app.post('/test-user', async (req, res) => {
@@ -40,7 +40,7 @@ app.post('/test-user', async (req, res) => {
       console.error('Error creating test user', e);
       res.status(500).send('Error creating test user');
    }
-})
+});
 
 // get all test user api
 app.get("/test-user", async (req, res) =>{
@@ -52,16 +52,38 @@ app.get("/test-user", async (req, res) =>{
       console.error('Error getting all test user', e);
       res.status(500).send('Error getting all test user');
    }
-})
+});
 
+// get tesst user by id api
 app.get("/test-user/:id", async (req, res) =>{
    try{
       const {id} = req.params;
-      const testUsers = await TestUser.findById(id);
-      res.status(200).json(testUsers);
+      const testUser = await TestUser.findById(id);
+      res.status(200).json(testUser);
    }
    catch(e){
       console.error('Error finding test user by id', e);
       res.status(500).send('Error finding test user by id');
    }
-})
+});
+
+
+// update test user by id api
+app.put("/test-user/:id", async (req, res) =>{
+   try{
+      const {id} = req.params;
+      const testUser = await TestUser.findByIdAndUpdate(id, req.body);
+
+      if(!testUser){
+         return res.status(404).send({message: "test user not found"});
+      }
+
+      const updatedtestUser = await TestUser.findById(id);
+      res.status(200).json(updatedtestUser);
+   }
+   catch(e){
+      console.error('Error updating test user', e);
+      res.status(500).send('Error updating test user');
+   }
+});
+
