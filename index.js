6bea1bb249f7b789f97301884300a7d1c45c2684
rahad404 <rahad.0911@gmail.com>
2026-05-20 01:39:01 +0600

    add: connection to mongo db

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+// install dotenv and then add this line to use .env file variable in code
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const mongoose = require('mongoose');
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+mongoose.connect(process.env.MONGODB_URI)
+   .then(() => {
+      console.log('Connected to MongoDB');
+
+      // Start the server after successful connection to MongoDB
+      // but we could add it before the connection as well, it will work either way 
+      app.listen(port, () => {
+         console.log(`Example app listening on port ${port}`)
+      })
+   })
+   .catch((e) => {
+      console.error('Error connecting to MongoDB', e);
+   });
+
+app.get('/', (req, res) => {
+   res.send('express server is running!')
 })
