1f8cc40f5d34846e1ef21fbaf99f380e7010af9f
rahad404 <rahad.0911@gmail.com>
2026-05-26 23:22:56 +0600

    fix: cors didnt work back to previous

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 const app = express();
 
 // middleware
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
